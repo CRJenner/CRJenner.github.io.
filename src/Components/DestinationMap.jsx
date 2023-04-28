d093305bbf3723c386f2ba9e9e0330e209c73181
Chloe Jenner <crjdowning@hotmail.com>
2023-04-28 13:41:42 +0100

    map card edited with css

--- src/Components/DestinationMap.jsx
+++ src/Components/DestinationMap.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import Card from 'react-bootstrap/Card';
+import { ArrowReturnLeft } from "react-bootstrap-icons";
+import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import 'leaflet/dist/leaflet.css';
+
+
 
 const DestinationMap = (props) => {
+  const navigate = useNavigate();
+    const handleClick = () => {
+        navigate("/");
+    }
   const data = props.data;
 
   const zoom = 13;
@@ -10,23 +20,22 @@ const DestinationMap = (props) => {
     <div className="map-card">
       <Card>
         <Card.Body>
-        <div className="card-container">
-  <div className="map-container">
             <MapContainer
+style={{ height: '400px', width: '100%' }}
               center={[data.latitude, data.longitude]}
               zoom={zoom}
               attributionControl={false}
             >
+              <div className="map-container">
               <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
               <Marker position={[data.latitude, data.longitude]}>
                 <Popup>{data.name}</Popup>
-              </Marker>
-            </MapContainer>
-          </div>
-          </div>
-        </Card.Body>
+              </Marker></div>
+            </MapContainer> 
         <Card.Title>A location map of {data.title} in {data.locationname} </Card.Title>
-
+        <Button size= "small"variant= "dark" onClick={handleClick} >
+        <ArrowReturnLeft /> | Go back </Button>
+        </Card.Body>
       </Card>
     </div>
   );
